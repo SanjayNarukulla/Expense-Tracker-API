3668ef89011171cc81d6d0faf8dc4857b88a35a7
SanjayNarukulla <narukullasanjay@gmail.com>
2024-10-23 12:48:15 +0530

    Update code and remove .env file from tracking

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt"); // Import bcrypt for password hashing
+const bcrypt = require("bcryptjs"); // Import bcrypt for password hashing
 const { connectToDatabase, getDb } = require("./db"); // Import getDb
 const { createUser, findUserByUsername } = require("./user");
 
